1ccb07db973859baa08500a802f3e70a73f54af2
ouchi <misaki.ouchi17@gmail.com>
2023-08-30 16:11:19 +0900

    各商品ページ表示まで（レビュー未）

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const con = mysql.createConnection({
 // cssファイルの取得
 app.use(express.static("assets"));
 
-
 // データ取得
 
 // デフォルト
@@ -40,34 +39,52 @@ const goodsPriceAsc = "SELECT * from goods ORDER BY price ASC;";
 // 商品名50音
 const goodsName = "SELECT * from goods ORDER BY name ASC;";
 
+// レビューソート
+// 評価の高い順
+const reviewsDesc = "SELECT * from reviews ORDER BY evaluation DESC;";
+// 評価の低い順
+const reviewsAsc = "SELECT * from reviews ORDER BY evaluation ASC;";
+
 // データ一覧
 const data = goods + reviews + goodsPriceDesc + goodsPriceAsc + goodsName;
 
-
 // ホームページ
 app.get("/", (req, res) => {
-  con.query(
-    data,
-    function (err, results, fields) {
-      if (err) throw err;
-      res.render("index", {
-        goods: results[0],
-        reviews: results[1],
-        goodsPriceDesc: results[2],
-        goodsPriceAsc: results[3],
-        goodsName: results[4],
-      });
-    }
-  );
+  con.query(data, function (err, results, fields) {
+    if (err) throw err;
+    res.render("index", {
+      goods: results[0],
+      reviews: results[1],
+      goodsPriceDesc: results[2],
+      goodsPriceAsc: results[3],
+      goodsName: results[4],
+    });
+  });
 });
 
 // 商品ページ
+// app.get("/itemList/:id", (req, res) => {
+//   const sql = "SELECT * FROM goods WHERE id = ?";
+//   con.query(sql, [req.params.id], function (err, result, fields) {
+//     if (err) throw err;
+//     res.render("itemList", {
+//       goods: result,
+//     });
+//   });
+// });
+
 app.get("/itemList/:id", (req, res) => {
+  const goodsItem = `SELECT * FROM goods WHERE id = ${req.params.id};`;
+  const sql = goodsItem + reviews + reviewsDesc + reviewsAsc;
   con.query(
-    goods, [req.params.id], function (err, result, fields) {
+    sql,
+    function (err, results, fields) {
     if (err) throw err;
     res.render("itemList", {
-      goods: result,
+      goodsItem: results[0],
+      reviews: results[1],
+      reviewsDesc: results[2],
+      reviewsAsc: results[3],
     });
   });
 });
